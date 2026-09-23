667b88ec3c803575f3caf73b20d1d52d0ca301f3
kc-scripter <kauachavesdefreitas@gmail.com>
2026-09-23 15:34:20 -0300

    Fix Tauri 2.11 shell compatibility

--- desktop-current/src-tauri/src/main.rs
+++ desktop-current/src-tauri/src/main.rs
@@ -1,21 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::webview::{PermissionKind, PermissionResponse};
-
 fn main() {
     tauri::Builder::default()
-        .on_permission_request(|_, kind| match kind {
-            PermissionKind::DisplayCapture
-            | PermissionKind::Camera
-            | PermissionKind::Microphone
-            | PermissionKind::Autoplay => PermissionResponse::Allow,
-            _ => PermissionResponse::Default,
-        })
-        .plugin(
-            tauri_plugin_autostart::Builder::new()
-                .app_name("Lunira Screen")
-                .build(),
-        )
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("failed to run Lunira Screen");
 }
