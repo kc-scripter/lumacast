fb4ac2f74d5f7dac6a84c6b6f5e3b42fd3c0aef8
kc-scripter <kauachavesdefreitas@gmail.com>
2026-09-23 15:10:51 -0300

    Allow desktop media permissions for screen publishing

--- desktop-current/src-tauri/src/main.rs
+++ desktop-current/src-tauri/src/main.rs
@@ -1,7 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::webview::{PermissionKind, PermissionResponse};
+
 fn main() {
     tauri::Builder::default()
+        .on_permission_request(|_, kind| match kind {
+            PermissionKind::DisplayCapture
+            | PermissionKind::Camera
+            | PermissionKind::Microphone
+            | PermissionKind::Autoplay => PermissionResponse::Allow,
+            _ => PermissionResponse::Default,
+        })
         .plugin(
             tauri_plugin_autostart::Builder::new()
                 .app_name("Lunira Screen")
