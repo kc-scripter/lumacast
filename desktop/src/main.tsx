f5604d74dfd1f280a523261e48cf8117536f3c6f
kc-scripter <kauachavesdefreitas@gmail.com>
2026-09-24 19:55:30 -0300

    fix(desktop): add Electron desktop capture bridge

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "../../client/src/components/ErrorBoundary";
 import { App } from "./App";
+import { installDesktopCaptureBridge } from "./services/desktopCaptureBridge";
 import "./styles.css";
+
+installDesktopCaptureBridge();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
